e8dd52bf8531210b1a4d036733b7abb45c0a9b99
GT <galtamas777@gmail.com>
2026-04-10 08:29:12 +0200

    Project finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://gtastrolearning.netlify.app"
+  site: "https://gtastrolearning.netlify.app",
+  integrations: [preact()]
 });
